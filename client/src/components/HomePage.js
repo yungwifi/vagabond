1d700e7ef21fbb55ad0db9c557e5e2fe1b088561
1tyler1 <mya12ga@yahoo.com>
2018-05-18 15:37:36 -0400

    updating homepage styling

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import styled from 'styled-components'
+
+const splash = styled.div`
+img{
+    align-self: center;
+    display: flex;
+}
+`
 
 class HomePage extends Component {
     render() {
         return (
+            <splash>
             <div>
+            <img src="Atlanta.jpg" alt="atlanta" />
                 Vagabond home page.
             </div>
+            </splash>
         )
     }
 }
