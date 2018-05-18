b619768ac90b531d630ee2de229bfabba50f836f
yungwifi <smerryman@me.com>
2018-05-18 14:37:40 -0400

    added homepage / installed node modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import HomePage from './components/HomePage'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+          </Switch>
+        </div>
+      </Router>
+    )
   }
 }
 
---
+++ client/src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                This is the home page.
+            </div>
+        )
+    }
+}
+
+export default HomePage
