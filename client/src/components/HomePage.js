9a6b2c0620dba980fdf2135b6c2563f7affbde55
1tyler1 <mya12ga@yahoo.com>
2018-05-18 16:40:31 -0400

    updating splash

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,22 +2,37 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import styled from 'styled-components'
 
-const splash = styled.div`
+const Splash = styled.div`
+display: flex;
+
+
 img{
-    align-self: center;
-    display: flex;
+    width: 100vw;
+    
+}
+nav{
+ background-color: #2E4A62;
 }
 `
 
 class HomePage extends Component {
     render() {
         return (
-            <splash>
+           <Splash>
             <div>
-            <img src="Atlanta.jpg" alt="atlanta" />
-                Vagabond home page.
+            <nav>
+    <div class="nav-wrapper z-depth-4">
+      <a href="#" class="brand-logo">Vagabond</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sign Up</a></li>
+        <li><a href="badges.html">Log In</a></li>
+      </ul>
+    </div>
+  </nav>
+  <img src='atlanta.jpg' alt="atlanta" />  
             </div>
-            </splash>
+         </Splash>
+         
         )
     }
 }
