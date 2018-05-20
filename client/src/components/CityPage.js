35f6755179bc201f786c78812fbc4e49439aca27
1tyler1 <mya12ga@yahoo.com>
2018-05-20 17:07:54 -0400

    updated styling for profile page

--- client/src/components/CityPage.js
+++ client/src/components/CityPage.js
@@ -2,14 +2,10 @@ import React, { Component } from 'react'
 import Photo1 from './images/london.jpg'
 import Photo2 from './images/san_francisco.jpg'
 import Photo3 from './images/photo2.jpg'
-import { Parallax, Background } from 'react-parallax'
-import CityVerbiage from './CityVerbiage'
+import { Parallax } from 'react-parallax'
 
-const insideStyles = {background: 'white', padding: 20, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)'};
-const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
-};
+
+const Styles = {background: 'white', padding: 20, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)'};
 
 class CityPage extends Component {
     render() {
@@ -19,23 +15,23 @@ class CityPage extends Component {
           <Parallax bgImage={Photo1}
             strength={500}>
             <div style={{height: 500}}>
-              <div style={insideStyles}>London</div>
+              <div style={Styles}>London</div>
             </div>
           </Parallax>
-          <h2>See posts for London</h2>
+          <a href="/cities/:cityId/posts"><h2>See posts for London</h2></a>
           <Parallax bgImage={Photo2} strength={400}>
             <div style={{height: 500}}>
-              <div style={insideStyles}>San Francisco</div>
+              <div style={Styles}>San Francisco</div>
             </div>
           </Parallax>
-          <h2>See posts for San Fran</h2>
+          <a href="/cities/:cityId/posts"><h2>See posts for San Fran</h2></a>
           <Parallax bgImage={Photo3} strength={300}>
             <div style={{height: 500}}>
-              <div style={insideStyles}>Atlanta</div>
+              <div style={Styles}>Atlanta</div>
             </div>
           </Parallax>
          
-            <h2>See posts for Atlanta</h2>
+          <a href="/cities/:cityId/posts"> <h2>See posts for Atlanta</h2></a>
         </div>
         )
     }
